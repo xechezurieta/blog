11ad749adbe187318164228221352cda2631bf40
Xabier Echezurieta <xabier.echezurieta@gmail.com>
2025-05-03 12:57:34 +0200

    feat: add category relationship to Post entity and update Category entity to manage posts

--- src/test/java/com/xechezurieta/blog/domain/entities/Category.java
+++ src/test/java/com/xechezurieta/blog/domain/entities/Category.java
@@ -3,6 +3,8 @@ package com.xechezurieta.blog.domain.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -21,6 +23,9 @@ public class Category {
 
     @Column(nullable = false, unique = true)
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Post> posts = new ArrayList<>();
 
     @Override
     public boolean equals(Object o) {
--- src/test/java/com/xechezurieta/blog/domain/entities/Post.java
+++ src/test/java/com/xechezurieta/blog/domain/entities/Post.java
@@ -38,6 +38,10 @@ public class Post {
     @JoinColumn(name = "author_id", nullable = false)
     private User author;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
