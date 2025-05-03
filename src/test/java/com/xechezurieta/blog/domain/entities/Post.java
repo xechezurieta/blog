04d81f5cf9a8b0f1d545bb64128635a00115718a
Xabier Echezurieta <xabier.echezurieta@gmail.com>
2025-05-03 12:54:43 +0200

    feat: add author relationship to Post entity and update User entity to manage posts

--- src/test/java/com/xechezurieta/blog/domain/entities/Post.java
+++ src/test/java/com/xechezurieta/blog/domain/entities/Post.java
@@ -34,6 +34,10 @@ public class Post {
     @Column(nullable = false)
     private Integer readingTime;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
--- src/test/java/com/xechezurieta/blog/domain/entities/User.java
+++ src/test/java/com/xechezurieta/blog/domain/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -28,6 +30,9 @@ public class User {
 
     @Column(nullable = false)
     private String name;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
