c60c924817b97215ee9e3d85508a695295968dd4
Xabier Echezurieta <xabier.echezurieta@gmail.com>
2025-05-03 12:37:01 +0200

    feat: add Tag entity with fields and lifecycle methods

---
+++ src/test/java/com/xechezurieta/blog/domain/entities/Tag.java
@@ -0,0 +1,36 @@
+package com.xechezurieta.blog.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tags")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(id, tag.id) && Objects.equals(name, tag.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
